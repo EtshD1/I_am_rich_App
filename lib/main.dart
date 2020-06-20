946941024927cf1f983dc985f87027f40933a8c5
EtshD1 <H79Pain@Gmail.com>
2020-06-20 19:08:59 +0200

    Finalized app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(RichApp());
@@ -10,8 +13,12 @@ class RichApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("I am Rich"),
+          title: Center(child: Text("I am Rich")),
           backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: SvgPicture.asset("images/ruby.svg"),
         ),
       ),
     );
